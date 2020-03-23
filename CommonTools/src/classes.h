1d010e30d63857bb694f646e49091c32955b0c8e
Pietro Vischia <pietro.vischia@gmail.com>
2020-03-23 14:29:16 +0100

    Forgotten to upload the new class def files

---
+++ CommonTools/src/classes.h
@@ -0,0 +1,7 @@
+#include "CombinedEWKAnalysis/CommonTools/interface/RooACProcessScaling_1D.h"
+#include "CombinedEWKAnalysis/CommonTools/interface/RooACProcessScaling_2D.h"
+#include "CombinedEWKAnalysis/CommonTools/interface/RooACProcessScaling_3D.h"
+#include "CombinedEWKAnalysis/CommonTools/interface/RooACSemiAnalyticPdf_1D.h"
+#include "CombinedEWKAnalysis/CommonTools/interface/RooACSemiAnalyticPdf_2D.h"
+#include "CombinedEWKAnalysis/CommonTools/interface/RooACSemiAnalyticPdf_3D.h"
+
